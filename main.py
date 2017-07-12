620c331bd91a00d43159937b8705eb2e222df388
Sun Baus <wobaus@gmail.com>
2017-07-12 00:16:31 -0700

    structured templates

--- main.py
+++ main.py
@@ -14,47 +14,48 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(300))
 
-    def __init__(self,title,body):
+    def __init__(self, title, body):
         self.title = title
         self.body = body
 
-@app.route('/blog', methods=['POST', 'GET'])
+@app.route('/blog')
 def blog():
     title = ""
     body = ""
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+    new_posting = ""
+    postings = ""
 
-        new_posting = Blog(title, body)
-        db.session.add(new_posting)
-        db.session.commit()
 
-    new_postings = Blog.query.all()
-    return render_template('blog.html',page_title="Build a Blog", title=title, body=body )
+    #if request.method == 'POST':
+    #title = request.form['title']
+    #body = request.form['body']
+    title = request.form.get("title")
+    body = request.form.get("body")
+    new_posting = Blog(title, body)
+
+    postings = Blog.query.all()
+
+    return render_template('blog.html',page_title="Build a Blog", title=title, body=body, new_posting=new_posting, postings=postings )
+  
 
 
 
 
 @app.route('/newpost', methods=['POST','GET'])
 def newpost():
-    title = ""
-    body = ""
+
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        
-        if title and body:
-            new_posting = Blog(title, body)
-            db.session.add(new_posting)
-            db.session.commit()
-            return redirect('/blog')
-        
-        else:
-            flash('please fill contents','error')
-    
-    
-    return render_template('newpost.html', page_title="Add a Blog Entry")
+        new_posting = Blog(title, body)
+        db.session.add(new_posting)
+        db.session.commit()
+        return redirect('/blog')
+
+    return render_template('/newpost.html',page_title="Add a blog Entry" )
+
+
+
 
 if __name__ == '__main__':
     app.run()
