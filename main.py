7f39b67dcd0e03b658700b1b103d60a8239ed022
Sun Baus <wobaus@gmail.com>
2017-07-20 19:36:10 -0700

    worked most of func besides display user and link

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'somekindofsecretkey'
 
-
 class Blog(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(300))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))                 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+             
 
 
     def __init__(self, title, body, owner):
@@ -28,20 +27,20 @@ class Blog(db.Model):
         self.owner = owner
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='owner')
+    blogs = db.relationship('Blog', backref='user')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-#@app.before_request
+
+@app.before_request
 def require_login():
     allowed_routes = ['login', 'signup']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
         
@@ -57,10 +56,9 @@ def login():
             session['username'] = username
             return redirect('/newpost')
         else:
-            return '<h1> Incorrect username or password </h1>'
-            #flash ('User password incorrect, or user does not exist')
-            #TODO specify each error message
-            
+            flash ('User password incorrect, or user does not exist')
+            return render_template('login.html', page_title="Log In!")
+    
     return render_template('login.html', page_title="Log In!")
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -85,7 +83,7 @@ def signup():
             if password != verifypassword:
                 flash('Password dont match')
             
-            return render_template('signup.html', page_title="SIGN UP!")
+                return render_template('signup.html', page_title="SIGN UP!")
 
         
             new_user = User(username,password)
@@ -103,21 +101,26 @@ def signup():
 def blog():
     
     posting_id = request.args.get('id')
-    auther = request.args.get('username')
-    print (auther)
+    author = request.args.get('username')
+    
+    # TODO: display and create link for posting username
     if posting_id: #grab posting id to display individual post
         current_page = Blog.query.get(posting_id)
+        #current_user = User.query.get(current_page.owner_id)
+        #get User class info of what current_page owner_id = User id
+
         return render_template('singleuser.html',current_page=current_page)
 
-    # TODO: how to get User.username from Blog.owner_id?
-    if auther: #get username to display all posts from this user
-        user = User.query.filter_by(username=auther).first()
+   
+    # if author: #get username to display all posts from this user
+    #     user = User.query.filter_by(username=author).first() #get selected user 
         
-        current_owner_post = Blog.query.filter_by(owner_id=user.id).first()
-        return render_template('blog.html',current_owner_post=current_owner_post, user=user)
+    #     current_owner_post = Blog.query.filter_by(owner_id=user.id).all() #try to get all data that matches owner_id in Blog class and id in user
+    #     return render_template('blog.html', user=user)
 
     else:
- 
+
+
         postings = Blog.query.all()
 
         return render_template('blog.html',page_title="All Postings",postings=postings)
